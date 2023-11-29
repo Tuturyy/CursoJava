86e9dba39e132bf24d1859d3e24fb6ce0c8ffa9a
Tuturyy <artur.aoliveir@gmail.com>
2023-11-29 18:55:54 +0000

    main adicionado

--- main.java
+++ main.java
@@ -3,5 +3,19 @@ import java.util.Scanner;
 public class main{
     public static void main(String[] args) {
         
+        //Criar alunos
+         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Quantos alunos deseja criar? ");
+        int quantidadeAlunos = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < quantidadeAlunos; i++) {
+            Aluno novoAluno = Aluno.criarNovoAluno(scanner);
+            Aluno.listaAlunos.add(novoAluno);
+        }
+
+        Aluno.listarAlunos();
+
+        scanner.close(); 
     }
 }
